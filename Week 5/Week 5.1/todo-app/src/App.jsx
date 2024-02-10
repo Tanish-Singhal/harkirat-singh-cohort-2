801dc48b62007a8edf5d539d69a91acd7c66ccf2
Tanish Singhal <tanishsinghal510@gmail.com>
2024-02-10 23:59:00 +0530

    basic frontend of todo app using react

---
+++ Week 5/Week 5.1/todo-app/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([{
+    title: "Go to gym",
+    description: "Go to gym from 7-9",
+    completed: false
+  }, {
+    title: "Study DSA",
+    description: "Study DSA from 9-10",
+    completed: true
+  }]);
+
+  function addTodo() {
+    // [1, 2]
+    // [...todos, 3] => [1, 2, 3]
+    setTodos([...todos, {
+      title: "new TODO",
+      description: "desc of new TODO"
+    }])
+  }
+
+  return (
+    <div>
+      
+      {/* TODO: {todos}   but this can't be worked because it is an array and this approach only works with the string */}
+      {/* {JSON.stringify(todos)} */}
+
+      <button onClick={addTodo}>Add a new TODO</button>
+
+      {/* <TODO title={todos[0].title} description={todos[0].description} />
+      <TODO title={todos[1].title} description={todos[1].description} /> */}
+      {/* TODO: but this approach was not scalable */}
+
+      {todos.map(function(todo) {
+        return <TODO title={todo.title} description={todo.description} />
+      })}
+
+    </div>
+  )
+}
+
+// component
+function TODO(props) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h3>{props.description}</h3>
+    </div>
+  )
+}
+
+export default App
