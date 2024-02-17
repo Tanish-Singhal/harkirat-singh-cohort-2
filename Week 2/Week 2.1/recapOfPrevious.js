31357437653d6ff5cbd16c5bc979eb56390de088
Tanish Singhal <tanishsinghal510@gmail.com>
2024-02-18 01:57:10 +0530

    strengthing the old concept and fix the issue

--- Week 2/Week 2.1/recapOfPrevious.js
+++ Week 2/Week 2.1/recapOfPrevious.js
@@ -3,6 +3,8 @@
 //     return n * n;
 // }
 
+const { create } = require("domain");
+
 // function cube(n) {
 //     return n * n * n;
 // }
@@ -63,20 +65,69 @@
 // }, 1000)
 
 
-function promisifiedMyOwnSetTimeout(duration) {
-    const p = new Promise(function(resolve) {
-        setTimeout(function() {
-            resolve();
-        }, duration);
+// function promisifiedMyOwnSetTimeout(duration) {
+//     const p = new Promise(function(resolve) {
+//         setTimeout(function() {
+//             resolve();
+//         }, duration);
+//     });
+//     return p;
+// }
+
+// const ans = promisifiedMyOwnSetTimeout(1000);
+// console.log(ans);
+// ans.then(function() {
+//     console.log("timeout is done");
+// });
+
+// ----------------------------------------------------------------
+
+const cart = ["shoes", "pants", "kurta"];
+
+const promise = createOrder(cart);
+
+promise
+.then(function(orderId) {
+    console.log(orderId);
+    return orderId;
+})
+.then(function(orderId) {
+    return proceedToPayment(orderId);
+})
+.then(function(paymentInfo) {
+    console.log(paymentInfo);
+})
+.catch(function(err) {
+    console.log(err.message);
+})
+
+function createOrder(cart) {
+    const pr = new Promise(function(resolve, reject) {
+        if (!validateCart(cart)) {
+            const err = new Error ("Cart is not valid");
+            reject(err);
+        }
+
+        const orderId = "12345";
+        if (orderId) {
+            setTimeout(function() {
+                resolve(orderId);
+            }, 5000);
+        }
     });
-    return p;
+
+    return pr;
 }
 
-const ans = promisifiedMyOwnSetTimeout(1000);
-console.log(ans);
-ans.then(function() {
-    console.log("timeout is done");
-});
+function proceedToPayment(orderId) {
+    return new Promise(function(resolve, reject) {
+        resolve("Payment Successful")
+    })
+}
+
+function validateCart(cart) {
+    return true;
+}
 
 // -----------------------------------------------------------------
 
