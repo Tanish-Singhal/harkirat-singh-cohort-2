10e075ddfcd1d6be4d1f889b870bc4172c95bfa2
Tanish Singhal <tanishsinghal510@gmail.com>
2024-02-19 00:06:22 +0530

    nothing much

--- Week 2/Week 2.1/recapOfPrevious.js
+++ Week 2/Week 2.1/recapOfPrevious.js
@@ -3,8 +3,6 @@
 //     return n * n;
 // }
 
-const { create } = require("domain");
-
 // function cube(n) {
 //     return n * n * n;
 // }
@@ -65,69 +63,20 @@ const { create } = require("domain");
 // }, 1000)
 
 
-// function promisifiedMyOwnSetTimeout(duration) {
-//     const p = new Promise(function(resolve) {
-//         setTimeout(function() {
-//             resolve();
-//         }, duration);
-//     });
-//     return p;
-// }
-
-// const ans = promisifiedMyOwnSetTimeout(1000);
-// console.log(ans);
-// ans.then(function() {
-//     console.log("timeout is done");
-// });
-
-// ----------------------------------------------------------------
-
-const cart = ["shoes", "pants", "kurta"];
-
-const promise = createOrder(cart);
-
-promise
-.then(function(orderId) {
-    console.log(orderId);
-    return orderId;
-})
-.then(function(orderId) {
-    return proceedToPayment(orderId);
-})
-.then(function(paymentInfo) {
-    console.log(paymentInfo);
-})
-.catch(function(err) {
-    console.log(err.message);
-})
-
-function createOrder(cart) {
-    const pr = new Promise(function(resolve, reject) {
-        if (!validateCart(cart)) {
-            const err = new Error ("Cart is not valid");
-            reject(err);
-        }
-
-        const orderId = "12345";
-        if (orderId) {
-            setTimeout(function() {
-                resolve(orderId);
-            }, 5000);
-        }
+function promisifiedMyOwnSetTimeout(duration) {
+    const p = new Promise(function(resolve) {
+        setTimeout(function() {
+            resolve();
+        }, duration);
     });
-
-    return pr;
+    return p;
 }
 
-function proceedToPayment(orderId) {
-    return new Promise(function(resolve, reject) {
-        resolve("Payment Successful")
-    })
-}
-
-function validateCart(cart) {
-    return true;
-}
+const ans = promisifiedMyOwnSetTimeout(1000);
+console.log(ans);
+ans.then(function() {
+    console.log("timeout is done");
+});
 
 // -----------------------------------------------------------------
 
