c124a3545f4db48dba5647e43d20652fe57a6939
Tanish Singhal <tanishsinghal510@gmail.com>
2024-02-20 22:35:17 +0530

    keys in react

---
+++ Week 6/Week 6.1/keys-memo-hooks/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// import ReactKeys from './components/ReactKeys'
+// import ReactMemo from './components/ReactMemo'
+import UseEffect from './components/UseEffect'
+
+
+function App() {
+  return (
+    <>
+      {/* <ReactKeys /> */}
+      {/* <ReactMemo /> */}
+      <UseEffect />
+    </>
+  )
+}
+
+export default App
---
+++ Week 6/Week 6.1/keys-memo-hooks/src/components/ReactKeys.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+let counter = 4;
+
+function ReactKeys() {
+  const [todos, setTodos] = useState([{
+    id: 1,
+    title: "go to gym",
+    description: "go to fym today"
+  }, {
+    id: 2,
+    title: "eat food",
+    description: "eat healthy food"
+  }, {
+    id: 3,
+    title: "go to class",
+    description: "go to web dev class"
+  }])
+
+  function addTodo() {
+    setTodos([...todos, {
+      id: counter++,
+      title: Math.random(),
+      description: Math.random()
+    }])
+  }
+
+  return (
+    <>
+      <button onClick={addTodo}>Add a Todo</button>
+      {todos.map(function (todo) {
+        return <Todo key={todo.id} title={todo.title} description={todo.description} />
+      })}
+      {/* Whenever the React is trying to do some operation on the divs, then it will be difficult for React to understand what element or div has to change */}
+    </>
+  )
+}
+
+// component
+function Todo({title, description}) {
+  return (
+    <>
+        <h2>{title}</h2>
+        <h5>{description}</h5>
+    </>
+  )
+}
+
+export default ReactKeys;
