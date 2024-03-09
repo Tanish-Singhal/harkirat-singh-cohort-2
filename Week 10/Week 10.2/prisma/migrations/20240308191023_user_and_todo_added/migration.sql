9df2ca65d1e875553aaaddb6c4d220c93bff48ce
Tanish Singhal <tanishsinghal510@gmail.com>
2024-03-09 23:20:20 +0530

    learning Prisma ORM

---
+++ Week 10/Week 10.2/prisma/migrations/20240308191023_user_and_todo_added/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT,
+    "firstName" TEXT,
+    "lastName" TEXT NOT NULL,
+    "phoneNumber" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL DEFAULT false,
+    "description" TEXT,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
---
+++ Week 10/Week 10.2/src/index.ts
@@ -0,0 +1,55 @@
+// in mongodb we do 
+// import mongoose from "mongoose"
+// mongoose.connect();
+
+
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+// after this you can use all Table (user and todo) and do operations on them
+
+// TODO: by this you can insert data in the Users Table 
+// async function insertUser(username: string, password: string, firstName: string, lastName: string) {
+//   const res = await prisma.user.create({
+//     data: {
+//       username,
+//       password,
+//       firstName,
+//       lastName
+//     },
+//     select: {
+//       id: true,
+//       password: true,
+//     }
+//   })
+//   console.log(res);
+// }
+// insertUser("tanish_singh_015", "password", "Tanish", "Singhal");
+
+// FIXME: if we run an errored query, then also the id increment
+
+
+
+// TODO: by this you can update the table 
+interface UpdateParams {
+  firstName: string;
+  lastName: string;
+}
+
+async function updateUser(username: string, {
+  firstName,
+  lastName
+}: UpdateParams) {
+  const res = await prisma.user.update({
+    where: { username },
+    data: {
+      firstName,
+      lastName
+    }
+  })
+  console.log(res);
+}
+updateUser('tanish_singh_015', {
+  firstName: "Tanish1111",
+  lastName: 'Singh'
+})
