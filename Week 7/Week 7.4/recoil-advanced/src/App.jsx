c99ed546760fef7d0298a506fd7f499aa9425d74
Tanish Singhal <tanishsinghal510@gmail.com>
2024-03-25 15:49:17 +0530

    advanced recoil concept

---
+++ Week 7/Week 7.4/recoil-advanced/src/App.jsx
@@ -0,0 +1,52 @@
+import { useMemo, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil'
+import { jobsAtom, notificationAtom, messageAtom, networkAtom, totalNotificationSelector } from './atoms'
+
+function App() {
+  return (
+    <RecoilRoot>
+      <MainApp />
+    </RecoilRoot>
+  )
+}
+
+function MainApp() { 
+  // const [networkCount, setNetworkCount] = useState(0)
+  // const [jobsCount, setJobsCount] = useState(0)
+  // const [messageCount, setMessageCount] = useState(0)
+  // const [notificationCount, setNotificationCount] = useState(0)
+  // FIXME: we can do the same thing using atoms
+
+  // RecoilValue and RecoilState
+  const networkNotificationCount = useRecoilValue(networkAtom);
+  const jobNotificationCount = useRecoilValue(jobsAtom);
+  const notificationCount = useRecoilValue(notificationAtom);
+  const [messageNotificationCount, setMessageNotificationCount] = useRecoilState(messageAtom);
+
+  // if you want to print the sum of all the notification count in the Me button
+  // const totalNotificationCount = useMemo(() => {
+  //   return networkNotificationCount + jobNotificationCount + notificationCount + messageNotificationCount;
+  // }, [networkNotificationCount, jobNotificationCount, messageNotificationCount, networkNotificationCount])
+  // FIXME: we can also do the same thing using Selector (with better approach)
+  const totalNotificationCount = useRecoilValue(totalNotificationSelector);
+
+  return (
+    <>
+      <button>Home</button>
+
+      <button>Network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button>
+      <button>Jobs ({jobNotificationCount >= 100 ? "99+" : jobNotificationCount})</button>
+      <button>Notifiction ({notificationCount >= 100 ? "99+" : notificationCount})</button>
+      <button>Message ({messageNotificationCount >= 100 ? "99+" : messageNotificationCount})</button>
+
+      <button onClick={() => {
+        setMessageNotificationCount(messageNotificationCount + 1);
+      }}>Me ({totalNotificationCount})</button>
+    </>
+  )
+}
+
+export default App
---
+++ Week 7/Week 7.4/recoil-advanced/src/atoms.jsx
@@ -0,0 +1,34 @@
+import { atom, selector } from 'recoil';
+
+export const networkAtom = atom({
+  key: "networkAtom",
+  default: 102
+});
+
+export const jobsAtom = atom({
+  key: "jobsAtom",
+  default: 0
+});
+
+export const messageAtom = atom({
+  key: "messageAtom",
+  default: 0
+});
+
+export const notificationAtom = atom({
+  key: "notificationAtom",
+  default: 12
+});
+
+
+// selector
+export const totalNotificationSelector = selector({
+  key: "totalNotificationSelector",
+  get: ({get}) => {
+    const networkAtomCount = get(networkAtom);
+    const jobsAtomCount = get(jobsAtom);
+    const messageAtomCount = get(messageAtom);
+    const notificationAtomCount = get(notificationAtom);
+    return networkAtomCount + jobsAtomCount + messageAtomCount + notificationAtomCount;
+  }
+})
