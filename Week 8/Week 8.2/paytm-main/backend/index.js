00d6f220c0d7652b6ebdc9a193b3371f94e4bb57
Tanish Singhal <tanishsinghal510@gmail.com>
2024-05-23 23:00:12 +0530

    signin and signup routes added

--- Week 8/Week 8.2/paytm-main/backend/index.js
+++ Week 8/Week 8.2/paytm-main/backend/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const mainRouter = require("./routes/index")
 const cors = require("cors");
 
-const app = express();
 app.use(cors());
 app.use(express.json());
+
+const mainRouter = require("./routes/index")
+const app = express();
 
 app.use("api/v1", mainRouter);
 
