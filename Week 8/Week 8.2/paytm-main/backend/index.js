043660f55952d382b65aaec51a4d06c6dc723f93
Tanish Singhal <tanishsinghal510@gmail.com>
2024-05-23 14:46:35 +0530

    initial setup of the project

--- Week 8/Week 8.2/paytm-main/backend/index.js
+++ Week 8/Week 8.2/paytm-main/backend/index.js
@@ -1,19 +1,7 @@
 const express = require("express");
-const cors = require("cors");
-const mainRouter = require("./routes/index");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
-
-
-// that's how we can add some prefix routes in all the URL
-app.use("/api/v1/", mainRouter);
-
-// all my routes are look like this
-// mainRouter (user)
-//  /api/v1/user/
-//  /api/v1/user/
 
 app.listen(3000);
