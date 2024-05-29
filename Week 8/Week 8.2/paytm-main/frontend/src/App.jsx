eb7fbde1a3980f09c20d12db45253df30bb7c971
Tanish Singhal <tanishsinghal510@gmail.com>
2024-05-29 17:59:53 +0530

    setting up routes for signup and sigin pages

--- Week 8/Week 8.2/paytm-main/frontend/src/App.jsx
+++ Week 8/Week 8.2/paytm-main/frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
 
 function App() {
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
