a149ebeb3c7dc1367e7aab8cc27a9b9d574964c5
Tanish Singhal <tanishsinghal510@gmail.com>
2024-05-30 23:33:50 +0530

    adding logout button

--- Week 8/Week 8.2/paytm-main/frontend/src/components/Appbar.jsx
+++ Week 8/Week 8.2/paytm-main/frontend/src/components/Appbar.jsx
@@ -1,19 +1,36 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Appbar = () => {
-  return <div className="shadow h-14 flex justify-between">
-      <div className="flex flex-col justify-center h-full ml-4">
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // use to remove the token generated from the local storage
+    localStorage.removeItem("token");
+
+    navigate("/signin");
+  };
+
+  return (
+    <>
+      <div className="shadow h-14 flex justify-between">
+        <h1 className="flex flex-col justify-center h-full ml-4 font-bold">
           PayTM App
-      </div>
-      <div className="flex">
-          <div className="flex flex-col justify-center h-full mr-4">
-              Hello
-          </div>
+        </h1>
+
+        <div className="flex">
+          <button
+            onClick={handleLogout}
+            className="flex flex-col justify-center h-full mr-4"
+          >
+            logout
+          </button>
+
           <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
-              <div className="flex flex-col justify-center h-full text-xl">
-                  U
-              </div>
+            <div className="flex flex-col justify-center h-full text-xl">U</div>
           </div>
+        </div>
       </div>
-  </div>
-}
+    </>
+  );
+};
