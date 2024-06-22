c99152372d3b4e1e70e15b145a1ec6203bc0b708
Tanish Singhal <tanishsinghal510@gmail.com>
2024-06-22 17:27:30 +0530

    adding user in database

--- Week 10/Week 10.2/src/index.ts
+++ Week 10/Week 10.2/src/index.ts
@@ -1,30 +1,25 @@
-// in mongodb we do 
-// import mongoose from "mongoose"
-// mongoose.connect();
-
-
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 // after this you can use all Table (user and todo) and do operations on them
 
 // TODO: by this you can insert data in the Users Table 
-// async function insertUser(username: string, password: string, firstName: string, lastName: string) {
-//   const res = await prisma.user.create({
-//     data: {
-//       username,
-//       password,
-//       firstName,
-//       lastName
-//     },
-//     select: {
-//       id: true,
-//       password: true,
-//     }
-//   })
-//   console.log(res);
-// }
-// insertUser("tanish_singh_015", "password", "Tanish", "Singhal");
+async function insertUser(username: string, password: string, firstName: string, lastName: string) {
+  const res = await prisma.user.create({
+    data: {
+      username,
+      password,
+      firstName,
+      lastName
+    },
+    select: {
+      id: true,
+      password: true,
+    }
+  })
+  console.log(res);
+}
+insertUser("tanish_singh_015", "password", "Tanish", "Singhal");
 
 // FIXME: if we run an errored query, then also the id increment
 
