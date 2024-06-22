2b5985b34319a06f2a55e93ea9f64404ff8c203e
Tanish Singhal <tanishsinghal510@gmail.com>
2024-06-22 17:23:43 +0530

    basic backend operation using hono

--- Week 11/Week 11.1/hono-app/src/index.ts
+++ Week 11/Week 11.1/hono-app/src/index.ts
@@ -2,9 +2,35 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+// TODO: simple route in hono
 // we can get both req, res using "c"
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+// app.get('/', (c) => {
+//   return c.text('Hello Hono!')
+// })
+
+
+// TODO: Middleware in hono
+async function authMiddleware(c: any, next: any) {
+  if (c.req.header("Authorization")) {
+    // Do validation
+    await next()
+  }
+  else {
+    return c.text("You dont have access");
+  }
+}
+app.use(authMiddleware);
+
+
+// TODO: how to access the body, headers and params in hono
+app.get("/", async (c) => {
+  const body = await c.req.json();
+
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
+  return c.text('Hello Hono!');
 })
 
 export default app
