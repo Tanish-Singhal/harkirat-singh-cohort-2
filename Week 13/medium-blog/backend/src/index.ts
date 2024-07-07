2ac63aa882c406ba5fadb30625aaed48c2ca8231
Tanish Singhal <tanishsinghal510@gmail.com>
2024-07-07 18:33:56 +0530

    signup route with JWT authentication

--- Week 13/medium-blog/backend/src/index.ts
+++ Week 13/medium-blog/backend/src/index.ts
@@ -1,6 +1,43 @@
+import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign } from 'hono/jwt'
 
-const prisma = new PrismaClient({
-    datasourceUrl: env.DATABASE_URL,
-}).$extends(withAccelerate())
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_SECRET: string,
+  }
+}>()
+
+app.post('/api/v1/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({
+      jwt: token,
+    });
+
+  } catch(e) {
+    c.status(403);
+    return c.json({
+      error: "error while signup up",
+    });
+  }
+})
+
+
+
+export default app
