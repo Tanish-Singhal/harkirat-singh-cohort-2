3504f3bfef40860825b605929979b4a705641270
Tanish Singhal <tanishsinghal510@gmail.com>
2024-07-08 19:08:07 +0530

    adding the signin and signup route

--- Week 13/medium-blog/backend/src/index.ts
+++ Week 13/medium-blog/backend/src/index.ts
@@ -1,11 +1,9 @@
 import { Hono } from 'hono'
 import { userRouter } from './routes/user';
-import { postRouter } from './routes/post';
 
 const app = new Hono();
 
 // TODO: Routing
 app.route('/api/v1/user', userRouter);
-app.route('/api/v1/post', postRouter);
 
 export default app
